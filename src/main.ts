8f98f3865a967b09282a1b4598086f8df22a9163
taq-fujihara <taq.fujihara@gmail.com>
2021-10-09 11:50:55 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import './style.css'
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+app.innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+
+class Foo {
+  bar() {
+    console.log('FOO!!!!!!!')
+  }
+}
+
+export { Foo }
+// export default Foo
