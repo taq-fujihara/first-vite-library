087618def449543fd88795581351be821a313a55
taq-fujihara <taq.fujihara@gmail.com>
2021-10-10 16:15:29 +0900

    working

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,11 @@
-import './style.css'
+import './style.scss'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
-
-class Foo {
+class Table {
   bar() {
     console.log('FOO!!!!!!!')
   }
 }
 
-export { Foo }
+export { Table }
 // export default Foo
